d6ee6b47edd4dd96bacb9c787be4bbfd5fce6826
Paiva <danpaiva@live.com>
2022-02-12 22:39:58 -0300

    add Flush()
    Former-commit-id: 085a8865f3523444db48b825d9fe97bc59098cb3

--- File_And_FileInfo/Program.cs
+++ File_And_FileInfo/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+
+var sw = File.CreateText(path);
+sw.WriteLine("Esta é a linha 01 do arquivo.");
+sw.WriteLine("Esta é a linha 02 do arquivo.");
+sw.WriteLine("Esta é a linha 03 do arquivo.");
+sw.Flush(); //Liberar a memória e escrever no arquivo
