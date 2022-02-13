ec00e9f8b6cdaea4dd1cf595ac48089098dea96b
Paiva <danpaiva@live.com>
2022-02-13 08:50:26 -0300

    Ajustes no programa
    Former-commit-id: a4bf83992d665f4e7b4cb49a9b28892bf05758f7

--- File_And_FileInfo/Program.cs
+++ File_And_FileInfo/Program.cs
@@ -1,6 +1,24 @@
-﻿var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+﻿using static System.Console;
 
-using var sw = File.CreateText(path);
-sw.WriteLine("Esta é a linha 01 do arquivo.");
-sw.WriteLine("Esta é a linha 02 do arquivo.");
-sw.WriteLine("Esta é a linha 03 do arquivo.");
+WriteLine("Digite o nome do arquivo: ");
+var nomeArquivo = ReadLine();
+var path = Path.Combine(Environment.CurrentDirectory, $"{nomeArquivo}.txt");
+CriarArquivo(path);
+
+WriteLine("Pressione alguma tecla para finalizar...");
+ReadLine();
+
+static void CriarArquivo(string path)
+{
+  try
+  {
+    using var sw = File.CreateText(path);
+    sw.WriteLine("Esta é a linha 01 do arquivo.");
+    sw.WriteLine("Esta é a linha 02 do arquivo.");
+    sw.WriteLine("Esta é a linha 03 do arquivo.");
+  }
+  catch
+  {
+    WriteLine("O nome do arquivo é inválido.");
+  }
+}
