be6d701b8968c3f0fbc96feceeaedd7e2bd4b6d0
Paiva <danpaiva@live.com>
2022-02-12 23:00:56 -0300

    Alterado o método para dissipar os recursos desnecessários
    Former-commit-id: 11a6a2b35796cc5986f37ebb2a0c8f03dc0a0bb5

--- File_And_FileInfo/Program.cs
+++ File_And_FileInfo/Program.cs
@@ -1,7 +1,6 @@
 ﻿var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
 
-var sw = File.CreateText(path);
+using var sw = File.CreateText(path);
 sw.WriteLine("Esta é a linha 01 do arquivo.");
 sw.WriteLine("Esta é a linha 02 do arquivo.");
 sw.WriteLine("Esta é a linha 03 do arquivo.");
-sw.Flush(); //Liberar a memória e escrever no arquivo
