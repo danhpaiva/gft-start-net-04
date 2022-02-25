44ea979363c4bd791c31728a146c3d4b8f6408af
Paiva <danpaiva@live.com>
2022-02-24 23:29:00 -0300

    Add programa que le diretorio
    Former-commit-id: cc135b82085545ffb1c320a60a25409e8f49dbd8

---
+++ LerDiretorio/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+var path = @"D:\work\gft-start-net-04\Directory_And_DirectoryInfo\globos";
+LerDiretorios(path);
+ReadKey();
+
+static void LerDiretorios(string path)
+{
+  if (Directory.Exists(path))
+  {
+    var diretorios = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+
+    foreach (var dir in diretorios)
+    {
+      var dirInfo = new DirectoryInfo(dir);
+      WriteLine($"Nome: {dirInfo.Name}");
+      WriteLine($"Raiz: {dirInfo.Root}");
+      if (dirInfo.Parent != null)
+        WriteLine($"Pai: {dirInfo.Parent.Name}");
+      WriteLine();
+    }
+  }
+  else
+  {
+    WriteLine($"Diretório {path} não existe");
+  }
+}
