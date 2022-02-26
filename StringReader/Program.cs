426e46de45a09136a7fa2a27a6e7e6e6c1155c6f
Paiva <danpaiva@live.com>
2022-02-26 18:11:34 -0300

    Conhecendo o StringReader
    Former-commit-id: 6d100def7147be5bc17f302a1f9e04e129f00ba1

--- StringReader/Program.cs
+++ StringReader/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using static System.Console;
+
+StringBuilder stringBuilder = new StringBuilder();
+stringBuilder.AppendLine("Texto na primeira linha a ser lido...");
+stringBuilder.AppendLine("Texto na segunda linha a ser lido...");
+stringBuilder.AppendLine("Texto na terceira linha a ser lido...");
+
+using var stringReader = new StringReader(stringBuilder.ToString());
+var buffer = new char[10];
+var tamanho = 0;
+
+LerArquivosPequenos(buffer, tamanho, stringReader);
+
+WriteLine("\nDigite ENTER para finalizar...");
+ReadLine();
+
+static void LerArquivosPequenos(char[] bufferzinho, int tamanho, StringReader stringReader)
+{
+  do
+  {
+    WriteLine(stringReader.ReadLine());
+  } while (stringReader.Peek() >= 0); //Retorna -1 quando acabar
+}
+
+static void LerArquivosGrandes(char[] bufferzinho, int tamanho, StringReader stringReader)
+{
+  do
+  {
+    bufferzinho = new char[10]; //Limpa o buffer para retirar a sujeira
+    tamanho = stringReader.Read(bufferzinho);
+    Write(string.Join("", bufferzinho));
+  } while (tamanho >= bufferzinho.Length);
+}
