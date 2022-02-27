10981bc6dc6c3b0746feb55810c5e33580383cfa
Paiva <danpaiva@live.com>
2022-02-27 19:46:04 -0300

    Trabalhando com o csvStream
    Former-commit-id: 332f88075acc0a9a0a85e4e76c66e90e863dd889

--- CsvStream/Program.cs
+++ CsvStream/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "usuarios.csv");
+
+if (File.Exists(path))
+{
+  using StreamReader streamReader = new StreamReader(path);
+
+  var cabecalho = streamReader.ReadLine()?.Split(',');
+
+  while (true)
+  {
+    var linha = streamReader.ReadLine()?.Split(',');
+    if (linha == null) break;
+
+    if (cabecalho.Length == linha.Length)
+    {
+      for (int i = 0; i < linha.Length; i++)
+      {
+        WriteLine($"{cabecalho?[i]}:{linha[i]}");
+      }
+      WriteLine();
+    }
+    else
+    {
+      WriteLine("Verifique o arquivo csv e suas delimitações.");
+      break;
+    }
+  }
+}
+else
+{
+  WriteLine("O arquivo não existe...");
+}
+
+
+
+WriteLine("Pressione Enter para finalizar...");
+ReadLine();
