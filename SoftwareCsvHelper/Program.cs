0cb83b67b02d4b697db2694cb24fe8e0e7148ffb
Paiva <danpaiva@live.com>
2022-02-27 20:55:59 -0300

    CsvHelper utilizando dynamic
    Former-commit-id: 349a9bb7c9e99f3668f50b33ce621bcdc510afe8

--- SoftwareCsvHelper/Program.cs
+++ SoftwareCsvHelper/Program.cs
@@ -1,2 +1,28 @@
-﻿// dotnet add package CsvHelper --version 27.2.1
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper;
+using static System.Console;
+
+// dotnet add package CsvHelper --version 27.2.1
+var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "produtos.csv");
+
+var fileInfo = new FileInfo(path);
+
+if (!fileInfo.Exists)
+  throw new FileNotFoundException($"O arquivo {path} não existe.");
+
+using var streamReader = new StreamReader(fileInfo.FullName);
+var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+using var csvReader = new CsvReader(streamReader, csvConfig);
+
+var registros = csvReader.GetRecords<dynamic>();
+
+foreach (var registro in registros)
+{
+  WriteLine($"Nome:{registro.Produto}");
+  WriteLine($"Marca:{registro.Marca}");
+  WriteLine($"Preço:{registro.Preco}");
+  WriteLine();
+}
+
+ReadLine();
