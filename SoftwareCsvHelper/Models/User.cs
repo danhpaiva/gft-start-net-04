5906125ea6500afd7e6772bbe7760911eae26473
Paiva <danpaiva@live.com>
2022-02-27 21:09:23 -0300

    Lendo arquivo com Csv e Classe
    Former-commit-id: 728d18341bc0d5a758bac7178fc6bc9205bce852

---
+++ SoftwareCsvHelper/Models/User.cs
@@ -0,0 +1,6 @@
+public class User
+{
+  public string Nome { get; set; }
+  public string Email { get; set; }
+  public long Telefone { get; set; }
+}
--- SoftwareCsvHelper/Program.cs
+++ SoftwareCsvHelper/Program.cs
@@ -4,25 +4,56 @@ using CsvHelper;
 using static System.Console;
 
 // dotnet add package CsvHelper --version 27.2.1
-var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "produtos.csv");
+//LerArquivoCsvComDynamic();
+LerCsvComClasse();
 
-var fileInfo = new FileInfo(path);
-
-if (!fileInfo.Exists)
-  throw new FileNotFoundException($"O arquivo {path} não existe.");
-
-using var streamReader = new StreamReader(fileInfo.FullName);
-var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
-using var csvReader = new CsvReader(streamReader, csvConfig);
-
-var registros = csvReader.GetRecords<dynamic>();
-
-foreach (var registro in registros)
+static void LerCsvComClasse()
 {
-  WriteLine($"Nome:{registro.Produto}");
-  WriteLine($"Marca:{registro.Marca}");
-  WriteLine($"Preço:{registro.Preco}");
-  WriteLine();
+  var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "users.csv");
+
+  var fileInfo = new FileInfo(path);
+
+  if (!fileInfo.Exists)
+    throw new FileNotFoundException($"O arquivo {path} não existe.");
+
+  using var streamReader = new StreamReader(fileInfo.FullName);
+  var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+  using var csvReader = new CsvReader(streamReader, csvConfig);
+
+  var registros = csvReader.GetRecords<User>();
+
+  foreach (var registro in registros)
+  {
+    WriteLine($"Nome:{registro.Nome}");
+    WriteLine($"Email:{registro.Email}");
+    WriteLine($"Telefone:{registro.Telefone}");
+    WriteLine();
+  }
 }
 
+static void LerArquivoCsvComDynamic()
+{
+  var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "produtos.csv");
+
+  var fileInfo = new FileInfo(path);
+
+  if (!fileInfo.Exists)
+    throw new FileNotFoundException($"O arquivo {path} não existe.");
+
+  using var streamReader = new StreamReader(fileInfo.FullName);
+  var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+  using var csvReader = new CsvReader(streamReader, csvConfig);
+
+  var registros = csvReader.GetRecords<dynamic>();
+
+  foreach (var registro in registros)
+  {
+    WriteLine($"Nome:{registro.Produto}");
+    WriteLine($"Marca:{registro.Marca}");
+    WriteLine($"Preço:{registro.Preco}");
+    WriteLine();
+  }
+}
+
+WriteLine("Digite Enter para finalizar...");
 ReadLine();
